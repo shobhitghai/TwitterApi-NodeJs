d41aeab516df6be14f28b4e7f90bf63182620e0b
shobhit <mail.shobhitghai@gmail.com>
2015-06-15 14:20:24 +0530

    changes for config.js

---
+++ config/config.js
@@ -0,0 +1,21 @@
+var config = function() {
+
+    this.consumer_key = function() {
+        return 'YeVgt7LVL0bP3YJRW2vrsm8vF';
+    }
+
+    this.consumer_secret = function() {
+        return '0At8eQy8Jb9pAKkCyCYVVPBPTbjwRXlkIk7IupLdMbFsyZ4HVV';
+    }
+
+    this.access_token_key = function() {
+        return '3186111847-pHPMcaSK0aEWQyGl61k8VJ4e0sV3FaiAtRUSKgS';
+    }
+
+    this.access_token_secret = function() {
+        return 'gSTZsrvieZkLbqcOiDWFwnDITa5790nA3HrtyQC0bBK8o';
+    }
+}
+
+
+module.exports = config;
